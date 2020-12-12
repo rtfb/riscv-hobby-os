4f03eafbe9e3b757e0c8671bd36da7bb5042e2aa
Vytautas Šaltenis <vytas@rtfb.lt>
2020-12-13 00:34:33 +0200

    Recursive fibonacci sequence func in assembly
    It works!
    
        fib(1) = 1
        fib(2) = 1
        fib(3) = 2
        fib(4) = 3
        fib(5) = 5
        fib(6) = 8
        fib(7) = 13
        fib(8) = 21
        fib(9) = 34
        fib(10) = 55
        fib(11) = 89
        fib(12) = 144
        fib(13) = 233
        fib(14) = 377

--- hello.c
+++ hello.c
@@ -2,9 +2,14 @@
 
 extern int returner();
 extern void asmprinter();
+extern int fib(int);
 
 void main() {
 	printf("hello, world\n");
 	printf("returner returned %d\n", returner());
 	asmprinter();
+
+    for (int i = 1; i < 15; i++) {
+        printf("fib(%d) = %d\n", i, fib(i));
+    }
 }
