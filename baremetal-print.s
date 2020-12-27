39c0b8dc853d7a642686eb0c71b4a835ced5d757
Renaldas Zioma <rej@unity3d.com>
2020-12-27 17:13:56 +0200

    Short documentation for the print.s functions. Made comments more readable

--- baremetal-print.s
+++ baremetal-print.s
@@ -12,9 +12,22 @@ MACHINE NOT SUPPORTED!
 .equ UART_REG_TXFIFO,   0
 
 .section .text
-.global printf
-                                        # IN: a0 = address of NULL terminated formatted string, a1 = address of argmuments
-                                        # formatting supports %s, %c, %d, %i, %u, %x, %o, %p
+.global printf                          # C-like print formatted string. Supports formatting: %s, %c, %d, %i, %u, %x, %o, %p
+                                        # IN: a0 = address of NULL terminated formatted string,
+                                        #     a1 = address of argmuments
+
+.global prints                          # Print string.
+                                        # IN: a0 = address of NULL terminated string
+
+.global printd                          # Print signed decimal number.
+                                        # IN: a0 = decimal number
+
+.global printu                          # Print unsigned decimal number.
+                                        # IN: a0 = decimal number
+
+.global printc                          # Print single character.
+                                        # IN: a0 = char
+
 .macro  push_printf_state
         addi    sp, sp, -3*REGBYTES
         sx      ra, 0,(sp)
@@ -63,41 +76,53 @@ printf:
 
 10:     li      t1, 'c'                 # if %c
         bne     t0, t1, 10f
-        # load char from a1, increment a1 by sizeof(char), print char
+                                        # load char from a1
+                                        # a1 += sizeof(char)
+                                        # print char
         call_printf_arg_handler printc, load_op32=lbu, load_op64=lbu, size=1
         j       0b                      # continue
 
 10:     li      t1, 's'                 # if %s
         bne     t0, t1, 10f
-        # load string pointer from a1, increment a1 by sizeof(char*), print null-terminated string
+                                        # load string pointer from a1
+                                        # a1 += sizeof(char*)
+                                        # print null-terminated string
         call_printf_arg_handler prints, load_op32=lw, load_op64=ld, size=REGBYTES
         j       0b                      # continue
 
 10:     li      t1, 'd'                 # if %d
         beq     t0, t1, 1f
-        li      t1, 'i'                 # if %i
+        li      t1, 'i'                 # or %i
         bne     t0, t1, 10f
-1:      # load int from a1, increment a1 by sizeof(int), print int
+1:                                      # load int from a1
+                                        # a1 += sizeof(int)
+                                        # print int
         call_printf_arg_handler printd, load_op32=lw, load_op64=lw, size=4
         j       0b                      # continue
 
 10:     li      t1, 'u'                 # if %u
         bne     t0, t1, 10f
-        # load unsigned int from a1, increment a1 by sizeof(unsigned), print unsigned
+                                        # load unsigned int from a1
+                                        # a1 += sizeof(unsigned)
+                                        # print unsigned
         call_printf_arg_handler printu, load_op32=lw, load_op64=lwu, size=4
         j       0b                      # continue
 
 10:     li      t1, 'o'                 # if %o
         bne     t0, t1, 10f
-        # load unsigned int from a1, increment a1 by sizeof(unsigned), print unsigned as octal
+                                        # load unsigned int from a1
+                                        # a1 += sizeof(unsigned)
+                                        # print unsigned as octal
         call_printf_arg_handler printo, load_op32=lw, load_op64=lwu, size=4
         j       0b                      # continue
 
 10:     li      t1, 'x'                 # if %x
         beq     t0, t1, 1f
-        li      t1, 'X'                 # if %X
+        li      t1, 'X'                 # or %X
         bne     t0, t1, 10f
-1:      # load unsigned int from a1, increment a1 by sizeof(unsigned), print unsigned as hexadecimal
+1:                                      # load unsigned int from a1
+                                        # a1 += sizeof(unsigned)
+                                        # print unsigned as hexadecimal
         call_printf_arg_handler printx, load_op32=lw, load_op64=lwu, size=4
         j       0b                      # continue
 
@@ -110,7 +135,9 @@ printf:
         li      a0, 'x'
         jal     printc                  # print '0x' in front of the address
         pop_printf_state
-        # load void* pointer from a1, increment a1 by sizeof(void*), print pointer address as hexadecimal
+                                        # load void* pointer from a1
+                                        # a1 += sizeof(void*)
+                                        # print pointer address as hexadecimal
         call_printf_arg_handler printx, load_op32=lw, load_op64=ld, size=REGBYTES
         j       0b                      # continue
 
@@ -141,8 +168,9 @@ prints:                                 # IN: a0 = address of NULL terminated st
 .global printd
 .global printu
 printd:                                 # IN: a0 = decimal number
-        # if input is negative,
-        # then print negative sign
+
+                                        # if input is negative,
+                                        # then print the negative sign first
         bgez    a0, printu
         neg     a0, a0                  # take two's complement of the input
         addi    sp, sp, -2*REGBYTES
@@ -163,9 +191,8 @@ print_radix:
         addi    a2, a2, -1
         sb      zero, 0(a2)             # null-terminate the string
 
-        # convert integer into the
-        # sequence of single digits
-        # and push them onto stack
+                                        # convert integer into the sequence of single digits
+                                        # and push them onto stack in the reversed order
 1:      remu    t0, a0, a1              # modulo radix
         li      t1, 10
         blt     t0, t1, 2f              # if t0 > 9
