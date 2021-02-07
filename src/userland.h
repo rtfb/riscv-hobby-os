f114b568a0f7bccbb31a1e5fcc90b13160c2565b
Vytautas Šaltenis <vytas@rtfb.lt>
2021-02-07 16:38:45 +0200

    Rewrite userland assembly inline

---
+++ src/userland.h
@@ -0,0 +1,10 @@
+#ifndef _USERLAND_H_
+#define _USERLAND_H_
+
+#define _userland __attribute__((__section__(".user_text")))
+
+extern void push_pop_stack();
+extern int *a_string_in_user_mem_ptr;
+extern int *msg_m_hello_ptr;
+
+#endif // ifndef _USERLAND_H_
