7fca4091f8d63654ee996e5471a295b06feac964
Vytautas Šaltenis <vytas@rtfb.lt>
2021-08-25 19:57:54 +0300

    Fix int64 definitions to be actually 64-bit
    Turns out an 'int' is 32-bit even on 64-bit targets. We need to have
    'long' for that. This manifested in a nasty bug in timer handling code
    which ended up overflowing timer value as soon as it reached 32-bit
    MAXINT (which happens to coincide with 7 minutes of working properly:
    7*60 seconds * 1e7 ticks/second).

--- include/sys.h
+++ include/sys.h
@@ -5,12 +5,12 @@
 // $ riscv64-linux-gnu-gcc -march=rv64g -mabi=lp64 -dM -E - < /dev/null | grep riscv
 #if __riscv_xlen == 32
     #define XLEN 32
-    #define int64_t long
-    #define uint64_t unsigned long
+    #define int64_t long long
+    #define uint64_t unsigned long long
 #elif __riscv_xlen == 64
     #define XLEN 64
-    #define int64_t int
-    #define uint64_t unsigned int
+    #define int64_t long
+    #define uint64_t unsigned long
 #else
     #error Unknown xlen
 #endif
