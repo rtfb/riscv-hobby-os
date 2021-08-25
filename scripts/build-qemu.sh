d066a240098294dfc63c533908ee2212a7c6b887
Vytautas Šaltenis <vytas@rtfb.lt>
2021-08-25 21:27:38 +0300

    Also build riscv32 qemu

--- scripts/build-qemu.sh
+++ scripts/build-qemu.sh
@@ -5,3 +5,6 @@ git checkout v5.1.0
 ./configure --target-list=riscv64-softmmu --enable-kvm --prefix=$(readlink -m ../qemu-build/)
 make -j $(nproc)
 make install
+./configure --target-list=riscv32-softmmu --enable-kvm --prefix=$(readlink -m ../qemu-build/)
+make -j $(nproc)
+make install
