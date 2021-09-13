4af752e4d9e4b012b2dfe2de36c20c776827d62a
Vytautas Šaltenis <vytas@rtfb.lt>
2021-09-13 20:04:40 +0300

    Cleanup: remove all the cruft related to running Linux
    Moved it to a separate repo, https://github.com/rtfb/linux-in-qemu.

--- scripts/build-busybox.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cd busybox
-CROSS_COMPILE=riscv64-linux-gnu- make defconfig
-sed -i '/CONFIG_STATIC/c\CONFIG_STATIC=y' .config
-CROSS_COMPILE=riscv64-linux-gnu- make -j $(nproc)
-CROSS_COMPILE=riscv64-linux-gnu- make install
