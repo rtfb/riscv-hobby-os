rtfb/riscv-hobby-os

commit 10628f998f0cd67c2443e2af10513b1f0793e26f

Implement parameter passing to kernel on unleashed machines
This adds a rudimentary Flattened Device Tree reader, which allows us
accessing the command line parameters for kernel, passed via qemu's
-append flag.

This in turn allows specifying which test cases we want to run. This
commit adds the first such test case: a "dry run" kernel with no
userland processes.

Unfortunately, E machines don't seem to implement device trees, so no
way to run tests on them yet.

src/kernel.c
@@ -1,16 +1,18 @@
 #include "sys.h"
 #include "kernel.h"
+#include "proc.h"
+#include "fdt.h"
 
-typedef struct process_s {
-    int pid;
-    void *pc;
-} process_t;
-
-process_t proc_table[2];
+process_t proc_table[MAX_PROCS];
 int curr_proc = 0;
+int num_procs = 0;
 
-void kinit() {
+void kinit(uintptr_t fdt_header_addr) {
     kprints("kinit\n");
+    fdt_init(fdt_header_addr);
+    kprints("bootargs: ");
+    kprints(fdt_get_bootargs());
+    kprints("\n");
     init_trap_vector();
     init_pmp();
     void *p = (void*)0xf10a; // this is a random hex to test out kprintp()
@@ -84,18 +86,19 @@ void kernel_timer_tick() {
 // called in interrupt_epilogue, after kernel_timer_tick() exits.
 void schedule_user_process() {
     curr_proc++;
-    if (curr_proc > 1) {
+    if (curr_proc >= num_procs) {
+        return;
+    }
+    if (curr_proc > MAX_PROCS-1) {
         curr_proc = 0;
     }
     set_jump_address(proc_table[curr_proc].pc);
     set_user_mode();
 }
 
+// TODO: move to proc.c
 void init_process_table() {
-    proc_table[0].pid = 0;
-    proc_table[0].pc = user_entry_point;
-    proc_table[1].pid = 1;
-    proc_table[1].pc = user_entry_point2;
+    init_test_processes();
     // init curr_proc to -1, it will get incremented to 0 on the first
     // scheduler run. This will also help to identify the very first call to
     // kernel_timer_tick, which will happen from the kernel land. We want to

.github/workflows/maketest.yml
@@ -17,7 +17,11 @@ jobs:
       run: ./scripts/install-ci.sh
     - name: Print xcompiler version
       run: riscv64-linux-gnu-gcc --version
-    - name: Run tests
+    - name: Run virt tests
       run: make test
+    - name: Run u64 tests
+      run: make out/test-output-u64.txt
+    - name: Run u32 tests
+      run: make out/test-output-u32.txt
     - name: Make all binaries
       run: make all

Makefile
@@ -50,7 +50,7 @@ runs: run-spike
 TEST_DEPS = src/baremetal-fib.s src/baremetal-print.s src/baremetal-poweroff.s
 USER_DEPS = src/boot.s src/baremetal-print.s \
 			src/baremetal-poweroff.s src/userland.c src/kernel.c src/syscalls.c \
-			src/pmp.c src/riscv.c
+			src/pmp.c src/riscv.c src/fdt.c src/string.c src/proc_test.c
 TEST_SIFIVE_U_DEPS = $(TEST_DEPS)
 USER_SIFIVE_U_DEPS = $(USER_DEPS)
 TEST_SIFIVE_E_DEPS = $(TEST_DEPS)
@@ -70,15 +70,34 @@ test: $(OUT)/test-output.txt
 $(OUT)/test-output.txt: $(OUT)/test_virt
 	@$(QEMU_LAUNCHER) --machine=virt --binary=$< > $@
 
+$(OUT)/test-output-u64.txt: $(OUT)/user_sifive_u
+	@$(QEMU_LAUNCHER) --bootargs dry-run --timeout=5s --binary=$< > $@
+	@diff -u testdata/want-output-u64.txt $@
+	@echo "OK"
+
+$(OUT)/test-output-u32.txt: $(OUT)/user_sifive_u32
+	@$(QEMU_LAUNCHER) --bootargs dry-run --timeout=5s --binary=$< > $@
+	@diff -u testdata/want-output-u32.txt $@
+	@echo "OK"
+
 .PHONY: run-baremetal
 run-baremetal: $(OUT)/user_sifive_u
+	$(QEMU_LAUNCHER) --binary=$<
+
+.PHONY: run-baremetale64
+run-baremetale64: $(OUT)/user_sifive_e
 	$(QEMU_LAUNCHER) --binary=$<
 
 .PHONY: run-baremetal32
 run-baremetal32: $(OUT)/user_sifive_e32
 	$(QEMU_LAUNCHER) --binary=$<
 
+.PHONY: run-baremetalu32
+run-baremetalu32: $(OUT)/user_sifive_u32
+	$(QEMU_LAUNCHER) --binary=$<
+
 GCC_FLAGS=-static -mcmodel=medany -fvisibility=hidden -nostdlib -nostartfiles \
+          -ffreestanding \
           -Tsrc/baremetal.ld -Iinclude
 
 $(OUT)/test_sifive_u: ${TEST_SIFIVE_U_DEPS}

include/fdt.h
@@ -1,0 +1,34 @@
+#ifndef _FDT_H_
+#define _FDT_H_
+
+#include "sys.h"
+
+// FDT header signature.
+#define FDT_MAGIC       0xd00dfeed
+
+// This is implemented against version 17 of the devicetree spec.
+#define FDT_VERSION     17
+
+#define FDT_BEGIN_NODE  0x00000001
+#define FDT_END_NODE    0x00000002
+#define FDT_PROP        0x00000003
+#define FDT_NOP         0x00000004
+#define FDT_END         0x00000009
+
+typedef struct fdt_header_s {
+    uint32_t magic;
+    uint32_t totalsize;
+    uint32_t off_dt_struct;
+    uint32_t off_dt_strings;
+    uint32_t off_mem_rsvmap;
+    uint32_t version;
+    uint32_t last_comp_version;
+    uint32_t boot_cpuid_phys;
+    uint32_t size_dr_strings;
+    uint32_t size_dt_struct;
+} fdt_header;
+
+void fdt_init(uintptr_t header_addr);
+char const* fdt_get_bootargs();
+
+#endif // ifndef _FDT_H_

include/kernel.h
@@ -11,6 +11,7 @@
 
 #define KERNEL_SCHEDULER_TICK_TIME (ONE_SECOND)
 
+void kinit(uintptr_t fdt_header_addr);
 void init_process_table();
 void init_trap_vector();
 void schedule_user_process();

include/proc.h
@@ -1,0 +1,21 @@
+#ifndef _PROC_H_
+#define _PROC_H_
+
+// TODO: move other process-related stuff here
+
+#define MAX_PROCS 2
+
+typedef struct process_s {
+    int pid;
+    void *pc;
+} process_t;
+
+extern process_t proc_table[MAX_PROCS];
+extern int num_procs;
+
+// init_test_processes initializes the process table with a set of userland
+// processes that will get executed by default. Kind of like what an initrd
+// would do, but a poor man's version until we can do better.
+void init_test_processes();
+
+#endif // ifndef _PROC_H_

include/string.h
@@ -1,0 +1,7 @@
+#ifndef _STRING_H_
+#define _STRING_H_
+
+int strncmp(char const *a, char const *b, unsigned int num);
+char* strncpy(char *dest, char const *src, unsigned int num);
+
+#endif // ifndef _STRING_H_

include/sys.h
@@ -7,13 +7,19 @@
     #define XLEN 32
     #define int64_t long long
     #define uint64_t unsigned long long
+    #define uint32_t unsigned long
+    #define uintptr_t unsigned long
 #elif __riscv_xlen == 64
     #define XLEN 64
     #define int64_t long
     #define uint64_t unsigned long
+    #define uint32_t unsigned int
+    #define uintptr_t unsigned long
 #else
     #error Unknown xlen
 #endif
+
+#define ARRAY_LENGTH(a) (sizeof(a) / sizeof(a[0]))
 
 extern void sys_puts(char const* msg);
 

commit 8610afbf4c172e1b2223a68d8e9aeaf047016d2e

Fix bug in round-robin scheduler
10628f9 has introduced a bug: our rudimentary scheduler now always
leaves one process out of the game. Fix the conditionals.

src/kernel.c
@@ -86,10 +86,10 @@ void kernel_timer_tick() {
 // called in interrupt_epilogue, after kernel_timer_tick() exits.
 void schedule_user_process() {
     curr_proc++;
-    if (curr_proc >= num_procs) {
+    if (num_procs == 0) {
         return;
     }
-    if (curr_proc > MAX_PROCS-1) {
+    if ((curr_proc >= MAX_PROCS) || (curr_proc >= num_procs)) {
         curr_proc = 0;
     }
     set_jump_address(proc_table[curr_proc].pc);