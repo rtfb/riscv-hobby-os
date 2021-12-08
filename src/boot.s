rtfb/riscv-hobby-os

commit c2fbe936bb200a748a5e279919265ad889667761

Clear the bss section before calling C codes
For the .bss section, it will contain the area of memory used to hold static variables which
must be initialized to zero. To ensure the correctness on device, we should clear the .bss
section before ever calling any C codes.

src/boot.s
@@ -34,6 +34,15 @@ _start:
 single_core:                            # only the 1st hart past this point
         la      sp, stack_top           # setup stack pointer
 
+        la      a0, bss_start
+        la      a1, bss_end
+        bgeu    a0, a1, init
+clean_bss_loop:
+        sw      zero, (a0)
+        addi    a0, a0, 4
+        bltu    a0, a1, clean_bss_loop
+init:
+
                                         # @TODO: check if user mode is supported
                                         # see: https://github.com/riscv/riscv-tests/blob/master/isa/rv64si/csr.S
                                         # OR alternatively use the trick of setting exception handler trailing the operation

src/baremetal.ld
@@ -23,10 +23,12 @@ SECTIONS
 
   . = DEFINED(ROM_START) ? RAM_START : .;
   .data : { *(.data) }
+  bss_start = .;
   .bss : {
     *(.sbss)
     *(.bss)
   }
+  bss_end = .;
   .sdata : { *(.sdata) }
   .debug : { *(.debug) }
   stack_bottom = .;

commit 52852638ac4d8154bc21425309f25241eb8f05fb

Fix build: parameter parsing via FDT got broken
c2fbe93 has introduced zeroing out of bss section, which trashes a1
register, which contains the address of FDT header on boot. The fix is
trivial: replace a0/a1 with t0/t1.

The breakage should've been caught by Github actions, but they don't run
automatically for the first-time contributors, and that's how it went
unnoticed.

src/boot.s
@@ -34,13 +34,13 @@ _start:
 single_core:                            # only the 1st hart past this point
         la      sp, stack_top           # setup stack pointer
 
-        la      a0, bss_start
-        la      a1, bss_end
-        bgeu    a0, a1, init
+        la      t0, bss_start
+        la      t1, bss_end
+        bgeu    t0, t1, init
 clean_bss_loop:
-        sw      zero, (a0)
-        addi    a0, a0, 4
-        bltu    a0, a1, clean_bss_loop
+        sw      zero, (t0)
+        addi    t0, t0, 4
+        bltu    t0, t1, clean_bss_loop
 init:
 
                                         # @TODO: check if user mode is supported