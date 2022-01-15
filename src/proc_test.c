8ba0e4034f64b5f5b353c45d5f935bcabd9c01bb
Vytautas Šaltenis <vytas@rtfb.lt>
2022-01-15 19:47:41 +0200

    Fixup 'Allocate user stack pages dynamically'

--- src/proc_test.c
+++ src/proc_test.c
@@ -11,9 +11,6 @@ void init_test_processes() {
     proc_table.num_procs = 2;
     proc_table.procs[0].pid = 0;
     proc_table.procs[0].pc = user_entry_point;
-    // For now, initialize userland stacks to hardcoded locations within the
-    // stack space: proc[0] will have its stack 128*XLEN_BYTES above
-    // stack_bottom
     void* sp = allocate_page();
     if (!sp) {
         // TODO: panic
@@ -22,7 +19,6 @@ void init_test_processes() {
     proc_table.procs[0].context.regs[REG_SP] = (regsize_t)(sp + PAGE_SIZE);
     proc_table.procs[1].pid = 1;
     proc_table.procs[1].pc = user_entry_point2;
-    // proc[1] will have its stack 128*XLEN_BYTES above proc[0]'s stack
     sp = allocate_page();
     if (!sp) {
         // TODO: panic
