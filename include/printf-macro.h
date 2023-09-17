9fa357df8fa18b7dc3562476e9b358606ec1c05c
Vytautas Šaltenis <vytas@rtfb.lt>
2023-09-17 18:32:02 +0300

    Move implementation of printfvecbuf to a macro
    This will be used to instantiate two separate implementations, one for
    the userland printf() and one for the kernel-side kprintf(). They need
    to be separate because we need them in different code segments. In
    addition, they will need independent ways to write their output.

---
+++ include/printf-macro.h
@@ -0,0 +1,92 @@
+#ifndef _PRINTF_MACRO_H_
+#define _PRINTF_MACRO_H_
+
+#define PRINTF_IMPL                                                    \
+    int argnum = 0;                                                    \
+    int srci = 0;                                                      \
+    int dsti = 0;                                                      \
+    const int rbufsz = 16;                                             \
+    char rbuf[rbufsz];                                                 \
+    while (fmt[srci] != 0) {                                           \
+        char c = fmt[srci];                                            \
+        if (c == '%' && argnum < nargs) {                              \
+            srci++;                                                    \
+            c = fmt[srci];                                             \
+            if (c == '%') {                                            \
+                buf[dsti] = '%';                                       \
+            } else {                                                   \
+                char const* tmp;                                       \
+                switch (c) {                                           \
+                    case 'c':                                          \
+                        ;                                              \
+                        char cb[2];                                    \
+                        cb[0] = args[argnum++];                        \
+                        cb[1] = 0;                                     \
+                        tmp = cb;                                      \
+                        break;                                         \
+                    case 'd':                                          \
+                        ;                                              \
+                        int i = args[argnum++];                        \
+                        tmp = _PRINT_RADIX(i, 10, rbuf, rbufsz);       \
+                        break;                                         \
+                    case 'x':                                          \
+                        ;                                              \
+                        int h = args[argnum++];                        \
+                        tmp = _PRINT_RADIX(h, 16, rbuf, rbufsz);       \
+                        break;                                         \
+                    case 's':                                          \
+                        tmp = (char const*)args[argnum++];             \
+                        break;                                         \
+                }                                                      \
+                for (; *tmp != 0; tmp++) {                             \
+                    buf[dsti] = *tmp;                                  \
+                    dsti++;                                            \
+                    if (dsti >= bufsize) {                             \
+                        return dsti;                                   \
+                    }                                                  \
+                }                                                      \
+                dsti--;                                                \
+            }                                                          \
+        } else {                                                       \
+            buf[dsti] = c;                                             \
+        }                                                              \
+        srci++;                                                        \
+        dsti++;                                                        \
+        if (dsti >= bufsize) {                                         \
+            return dsti;                                               \
+        }                                                              \
+    }                                                                  \
+    return dsti
+
+#define PRINT_RADIX_IMPL                                               \
+    int dsti = size - 1;                                               \
+    buf[dsti] = 0; /* terminate the string */                          \
+    dsti--;                                                            \
+    int limit = 0;                                                     \
+    int negative = 0;                                                  \
+    if (num == 0) {                                                    \
+        buf[dsti] = '0';                                               \
+        return &buf[dsti];                                             \
+    }                                                                  \
+    if (num < 0) {                                                     \
+        limit++; /* leave space for the minus sign */                  \
+        negative = 1;                                                  \
+        num = -num;                                                    \
+    }                                                                  \
+    while (num > 0 && dsti >= limit) {                                 \
+        int digit = num % radix;                                       \
+        if (digit > 9) {                                               \
+            buf[dsti] = 'a' + (digit - 10);                            \
+        } else {                                                       \
+            buf[dsti] = '0' + digit;                                   \
+        }                                                              \
+        num /= radix;                                                  \
+        dsti--;                                                        \
+    }                                                                  \
+    dsti++;                                                            \
+    if (negative) {                                                    \
+        buf[dsti] = '-';                                               \
+    }                                                                  \
+    return &buf[dsti]
+
+#endif // ifndef _PRINTF_MACRO_H_
--- user/src/user-printf.c
+++ user/src/user-printf.c
@@ -1,39 +1,19 @@
 #include "userland.h"
 #include "ustr.h"
+#include "pagealloc.h"
+#include "printf-macro.h"
 
 // print_radix prints a given number in a given radix. It stores the string in
 // a provided buffer, fills it starting from the end, and returns a pointer
 // into it, pointing to the beginning of the number.
 char const* _userland print_radix(regsize_t num, int radix, char *buf, unsigned int size) {
-    int dsti = size - 1;
-    buf[dsti] = 0; // terminate the string
-    dsti--;
-    int limit = 0;
-    int negative = 0;
-    if (num == 0) {
-        buf[dsti] = '0';
-        return &buf[dsti];
-    }
-    if (num < 0) {
-        limit++; // leave space for the minus sign
-        negative = 1;
-        num = -num;
-    }
-    while (num > 0 && dsti >= limit) {
-        int digit = num % radix;
-        if (digit > 9) {
-            buf[dsti] = 'a' + (digit - 10);
-        } else {
-            buf[dsti] = '0' + digit;
-        }
-        num /= radix;
-        dsti--;
-    }
-    dsti++;
-    if (negative) {
-        buf[dsti] = '-';
-    }
-    return &buf[dsti];
+    PRINT_RADIX_IMPL;
+}
+
+int32_t _userland printfvecbuf(char const* fmt, regsize_t* args, int nargs, char *buf, int bufsize) {
+    #define _PRINT_RADIX print_radix
+    PRINTF_IMPL;
+    #undef _PRINT_RADIX
 }
 
 // printfvec does formatted output. It formats the output into an internal
@@ -45,60 +25,13 @@ char const* _userland print_radix(regsize_t num, int radix, char *buf, unsigned 
 //
 // It supports the following verbs: %c, %d, %s, %x.
 int32_t _userland printfvec(char const* fmt, regsize_t* args) {
-    int argnum = 0;
     char* buf = (char*)pgalloc();
     if (!buf) {
         return -1;
     }
-    int srci = 0;
-    int dsti = 0;
-    const int rbufsz = 16;
-    char rbuf[rbufsz];
-    while (fmt[srci] != 0) {
-        char c = fmt[srci];
-        if (c == '%' && argnum < 7) {
-            srci++;
-            c = fmt[srci];
-            if (c == '%') {
-                buf[dsti] = '%';
-            } else {
-                char const* tmp;
-                switch (c) {
-                    case 'c':
-                        ;
-                        char cb[2];
-                        cb[0] = args[argnum++];
-                        cb[1] = 0;
-                        tmp = cb;
-                        break;
-                    case 'd':
-                        ;
-                        int i = args[argnum++];
-                        tmp = print_radix(i, 10, rbuf, rbufsz);
-                        break;
-                    case 'x':
-                        ;
-                        int h = args[argnum++];
-                        tmp = print_radix(h, 16, rbuf, rbufsz);
-                        break;
-                    case 's':
-                        tmp = (char const*)args[argnum++];
-                        break;
-                }
-                for (; *tmp != 0; tmp++) {
-                    buf[dsti] = *tmp;
-                    dsti++;
-                }
-                dsti--;
-            }
-        } else {
-            buf[dsti] = c;
-        }
-        srci++;
-        dsti++;
-    }
-    buf[dsti] = 0;
-    int32_t nwritten = write(1, buf, dsti);
+    int32_t buf_written = printfvecbuf(fmt, args, 7, buf, PAGE_SIZE-1);
+    buf[buf_written] = 0;
+    int32_t nwritten = write(1, buf, buf_written);
     pgfree(buf);
     return nwritten;
 }
