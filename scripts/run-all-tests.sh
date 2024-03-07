66c5d5a5488745e0758baba4fd866fccc04e6353
Vytautas Šaltenis <vytas@rtfb.lt>
2024-03-07 15:18:35 +0200

    Uncomment daemon tests on u64
    Now that we have a more stable way to wait on the daemon, we can
    uncomment the tests on 64-bit sifive_u.

--- scripts/run-all-tests.sh
+++ scripts/run-all-tests.sh
@@ -10,6 +10,5 @@ make out/smoke-test-output-u64.txt
 make out/smoke-test-output-virt.txt
 make out/smoke-test-output-tiny-stack.txt
 make out/daemon-test-output-virt.txt
-# Commented out for now because of a difference in output due to timing issues
-# make out/daemon-test-output-u64.txt
+make out/daemon-test-output-u64.txt
 make out/daemon-test-output-tiny-stack.txt
